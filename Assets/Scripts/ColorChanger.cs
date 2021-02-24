90ac1856076e7a24d6c03540dab0814d9ea93df1
babcocke1 <56487953+babcocke1@users.noreply.github.com>
2021-02-24 00:54:41 -0700

    added build/assets

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public Color[] colors = new Color[7];
+    public Color cur;
+    public Color next;
+    public int index;
+    public int nextIndex;
+    float startTime;
+    private void Start()
+    {
+        startTime = Time.time;
+    }
+
+    private void Update()
+    {
+        float t = (Time.time - startTime) * speed;
+        GetComponent<Renderer>().material.color = Color.Lerp(colors[index], colors[nextIndex], t);
+        if(t >= 1)
+        {
+            startTime = Time.time;
+            index++;
+            if(index == colors.Length)            
+                index = 0;
+            nextIndex++;
+            if(nextIndex == colors.Length)
+            {
+                nextIndex = 0;
+            }
+        }
+    }
+}
